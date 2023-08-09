18e42bae266d54e48b1457ee673269d17f155033
Ermeson23 <joseenem35@gmail.com>
2023-08-09 20:02:45 -0300

    Completing the utility class

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -1,3 +1,4 @@
+import { FormUtilsService } from './../../../shared/form/form-utils.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, NonNullableFormBuilder, UntypedFormArray, Validators } from '@angular/forms';
 
@@ -21,7 +22,8 @@ export class CourseFormComponent implements OnInit {
     private service: CoursesService,
     private snackBar: MatSnackBar,
     private location: Location,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+    public formUtils: FormUtilsService) {
    }
 
    ngOnInit(): void {
@@ -78,7 +80,7 @@ export class CourseFormComponent implements OnInit {
       this.service.save(this.form.value)
         .subscribe(result => this.onSuccess(), error => this.onError());
     } else {
-      alert('Formulário inválido');
+      this.formUtils.validateAllFormFields(this.form);
     }
    }
 
@@ -93,31 +95,6 @@ export class CourseFormComponent implements OnInit {
 
    onError() {
     this.snackBar.open('Erro ao salvar curso', '', { duration: 5000 });
-   }
-
-   getErrorMessage(fieldName: string) {
-    const field = this.form.get(fieldName);
-
-    if(field?.hasError('required')) {
-      return 'Campo Obrigatório.';
-    }
-
-    if(field?.hasError('minlength')) {
-      const requiredLength = field.errors ? field.errors['minlength']['requiredLength'] : 5;
-      return `Tamanho mínimo precisa ser de ${requiredLength} caracteres.`;
-    }
-
-    if(field?.hasError('maxlength')) {
-      const requiredLength = field.errors ? field.errors['maxlength']['requiredLength'] : 100;
-      return `Tamanho máximo excedido de ${requiredLength} caracteres.`;
-    }
-
-    return 'Campo inválido';
-   }
-
-   isFormArrayRequired() {
-    const lessons = this.form.get('lessons') as UntypedFormArray;
-    return !lessons.valid && lessons.hasError('required') && lessons.touched;
    }
 
 }
